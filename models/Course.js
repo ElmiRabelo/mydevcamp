c5d41221bafc0f6e30939276893cbcd36493ebbf
Elmi Rabelo <rabeloelmi@gmail.com>
2020-03-16 19:03:41 -0300

    Adicionado logica de custo médio nos bootcamps

--- models/Course.js
+++ models/Course.js
@@ -41,4 +41,37 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+//Metodo estatico para obter o custo médio de taxas dos cursos
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" }
+      }
+    }
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//Invocar getAverageCost depois de salvar
+CourseSchema.post("save", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Invocar getAverageCost antes de remover
+CourseSchema.pre("remove", function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
